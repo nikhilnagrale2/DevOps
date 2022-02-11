269059dc66228149f40009d4aa907a49068675e1
nikhilnagrale2 <nknagrale634@gmail.com>
2022-02-11 10:07:22 +0530

    added divide function

--- a.cpp
+++ a.cpp
@@ -1,4 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { return 0; }
+int calc_divide(int a, int b) { return a / b; }
+
+int main() {
+    int a, b;
+    a = 5, b = 2;
+    cout << calc_divide(a, b) << endl;
+    return 0;
+}
